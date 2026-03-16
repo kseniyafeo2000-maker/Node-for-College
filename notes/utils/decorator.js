aced17b6556af3932eb0987b8964eb91e6f6debc
xenimini <kseniya.feo2000@gmail.com>
2026-03-16 11:45:20 +0300

    Create decorator.js

---
+++ notes/utils/decorator.js
@@ -0,0 +1,13 @@
+class Decorator{
+  static drawLine(){
+    console.log("=".repeat(30));
+  }
+
+  static presentMenu(welcome){
+    this.drawLine();
+    console.log('\n');
+    console.log(`${welcome}`);
+    this.drawLine();
+  }
+}
+module.exports = Decorator;
