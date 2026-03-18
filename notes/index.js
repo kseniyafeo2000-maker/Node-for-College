2fdf3ed24384709eda386e45234ae369037a4e38
xenimini <kseniya.feo2000@gmail.com>
2026-03-18 08:43:28 +0300

    Update index.js

--- notes/index.js
+++ notes/index.js
@@ -1,26 +1,34 @@
-const readline = require("readline"); // импортируем модуль из node
-const helper = require("./utils/helper");
-const Decorator = require("./utils/decorator");
+//=============================
+// Осеовной скрипт приложения
+//=============================
 
+// импорты
+const readline = require("readline"); // импортируем модуль из node
+const helper = require("./utils/helper"); // импортируем свои модули 
+const Decorator = require("./utils/decorator");
+// инициализация ввода вывода
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
-
+// переменные
 const NAME_PROJ = '"NOTE"-"BOOK"';
-
 let notes = [];
-
 let welcome = `Тебя приветствует приложение ${NAME_PROJ}`;
 
+//=============================
+// Функции
+//=============================
+
+//приветствие
 const welcomeApp = () => {
-  Decorator.presentMenu(welcome);
+  Decorator.presentWelcome(welcome);
   showMenu();
 };
-
+//добавление заметки
 const addNote = () => {
-  rl.question("Введите заголовок", (title) => {
-    rl.question("Напишите текст заметки", (content) => {
+  rl.question("Введите заголовок  ", (title) => {
+    rl.question("Напишите текст заметки  ", (content) => {
       const newNote = {
         id: notes.length + 1,
         title: title,
@@ -29,33 +37,22 @@ const addNote = () => {
       };
       notes.push(newNote);
       console.log(`Заметка ${newNote.title} сохранена!`);
-      console.log(`Всего заметок ${notes.length}`);
 
       showMenu();
     });
   });
 }; 
-
+//просмотр всех заметок
 const showNotes = () => {
-  console.log("----Все ваши заметки----");
-  notes.forEach((note) => {
-    console.log("-".repeat(30));
-    console.log(`${note.id} * ${note.date}`);
-    console.log(`${note.title}`);
-    console.log(`${note.content}`);
-    console.log("-".repeat(30));
-  });
+  Decorator.showFormatAllNotes(notes);
   showMenu();
 };
-
+//меню программы
 const showMenu = () => {
-  console.log(`Всего заметок ${notes.length}`);
-  console.log("Главное меню");
-  console.log("1. Доюавить заметку");
-  console.log("2. Посмотреть заметки");
-  console.log("3. Удаление заметки");
+  helper.statsNotes(notes);
+  Decorator.presentMenu();
 
-  rl.question("Выберите пункт от 1 до 2", (choice) => {
+  rl.question("Выберите пункт от 1 до 4  ", (choice) => {
     switch(choice){
       case '1':
         addNote();
@@ -66,13 +63,17 @@ const showMenu = () => {
       case '3':
         deleteNote();
         break;
+      case '4':
+        console.log("Завершение работы!")
+        rl.close();
+        break;
       default:
         console.log("Нет такого пункта!");
         showMenu();
     };
   });
 };
-
+//удаление заметки
 const deleteNote = () => {
   if(notes.length === 0){
     console.log("У вас пока нет заметок!");
@@ -80,7 +81,7 @@ const deleteNote = () => {
   notes.forEach((note) => {
     console.log(`\n * [${note.id}] * ${note.title} *`);
   });
-  rl.question("Введите номер заметки для удаления или 0 для отмены", (choice) =>{
+  rl.question("Введите номер заметки для удаления или 0 для отмены  ", (choice) =>{
     let num = parseInt(choice);
     if(num === 0){
       showMenu();
@@ -98,5 +99,5 @@ const deleteNote = () => {
   });
   showMenu();
 };
-
+//запуск программы
 welcomeApp();
